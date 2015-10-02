e2564c1a64c199ab41370a1bb52a94fc586bea89
DuffmanCC <carlosor@gmail.com>
2015-10-02 18:55:33 +0200

    curso finalizado

--- curso_lynda/js/controllers.js
+++ curso_lynda/js/controllers.js
@@ -1,11 +1,45 @@
-var myApp = angular.module('myApp', []);
+var artistControllers = angular.module('artistControllers', []);
 
-myApp.controller('MyController', ['$scope', '$http', function($scope, $http) {
+artistControllers.controller('ListController', ['$scope', '$http', function($scope, $http) {
   $http.get('js/data.json').success(function(data) {
     $scope.artists = data;
     $scope.artistOrder = 'name';
   });
 }]);
+
+
+artistControllers.controller('DetailsController', ['$scope', '$http', '$routeParams', function($scope, $http, $routeParams) {
+  $http.get('js/data.json').success(function(data) {
+    $scope.artists = data;
+    $scope.whichItem = $routeParams.itemId;
+
+    if ( $routeParams.itemId > 0 ) {
+    	// si no me encuentro en el primero elemento
+    	// el boton prevItem me manda al elemento anterior.
+    	$scope.prevItem = Number($routeParams.itemId) - 1;
+    } else {
+    	// si me encuentro en el último elemento de la array
+    	// o sea, $routeParams.itemId = 0, entonces me manda
+    	// al último elemento de la array.
+    	$scope.prevItem = $scope.artists.length - 1;
+    };
+
+    if ( $routeParams.itemId < ($scope.artists.length - 1) ) {
+    	// si no estamos en el último elemento avanzamos 
+    	// al siguiente elemento.
+    	$scope.nextItem = Number($routeParams.itemId) + 1;
+    } else {
+    	// si nos encontramos en el último elemento 
+    	// vamos hasta el elemente [0] de la array.
+    	$scope.nextItem = 0;
+    };
+
+  });
+}]);
+
+
+
+
 	
 
 
